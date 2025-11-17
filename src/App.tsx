4330a72a28f87221df7f053d0c74c402fc56148f
Nasim <mdnasimhosen333@gmail.com>
2025-11-18 00:53:16 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-amber-700">
+      <Button className="bg-blue-900">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
